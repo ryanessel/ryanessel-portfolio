5dfea2b3602a94010d7524a54bd59347641212eb
Ryan Essel <spucti19@gmail.com>
2023-01-11 16:48:19 -0500

    adding Ironhack Certificate

---
+++ src/components/IronhackCert.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ function IronhackCert() {
+  return (
+    <div>
+        <a src="https://www.credential.net/embed/fae4126b-baaa-4b3d-8a0a-4f74db68b5fb">
+        <img src="https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/66180186"/>
+        </a>
+    </div>
+  )
+}
+
+export default IronhackCert;
